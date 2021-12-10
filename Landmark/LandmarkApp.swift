98947c37233af918590f9a5309f5a928e2f52b85
manab oz <staff@oz-edu.com>
2021-12-10 16:21:31 +0900

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by 辻大地 on 2021/12/10.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
