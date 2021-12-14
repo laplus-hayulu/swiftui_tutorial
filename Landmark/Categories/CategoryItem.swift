277fda381182059620e01d4f63a5971b4c48506e
potepote47 <kie@manalabo.jp>
2021-12-14 16:51:22 +0900

    add categoryitem.swift

---
+++ Landmark/Categories/CategoryItem.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryItem.swift
+//  Landmark
+//
+//  Created by 辻大地 on 2021/12/14.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem()
+    }
+}
