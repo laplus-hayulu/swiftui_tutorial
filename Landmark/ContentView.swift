0120bb88db64f893debd748a8936a0b988c439cb
potepote47 <kie@manalabo.jp>
2021-12-14 17:26:06 +0900

    finish swiftui-6

--- Landmark/ContentView.swift
+++ Landmark/ContentView.swift
@@ -1,11 +1,29 @@
 import SwiftUI
 
-
-
 struct ContentView: View {
-    var body: some View {
-        LandmarkList()
+    @State private var selection: Tab = .featured // 初期はFeaturedタブ
+    
+    //　Tabに関する列挙型を定義
+    enum Tab {
+        case featured
+        case list
     }
+    
+    var body: some View {
+            TabView(selection: $selection) {
+                CategoryHome()
+                    .tabItem {
+                        Label("Featured", systemImage: "star")
+                    }
+                    .tag(Tab.featured)
+     
+                LandmarkList()
+                    .tabItem {
+                        Label("List", systemImage: "list.bullet")
+                    }
+                    .tag(Tab.list)
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
