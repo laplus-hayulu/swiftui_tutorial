41e2e65186b1141d2ada0e46110637805c3819f1
potepote47 <kie@manalabo.jp>
2021-12-14 13:59:37 +0900

    add HexagonParameters.swift

---
+++ HexagonParameters.swift
@@ -0,0 +1,20 @@
+//
+//  HexagonParameters.swift
+//  Landmark
+//
+//  Created by 辻大地 on 2021/12/14.
+//
+
+import SwiftUI
+
+struct HexagonParameters: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HexagonParameters_Previews: PreviewProvider {
+    static var previews: some View {
+        HexagonParameters()
+    }
+}
